73100bfb6b83e51820e2906d7ab7d7a3ab9583f6
bagoessprasetyo <prasetyobagus7@gmail.com>
2023-07-23 20:04:30 +0700

    minify css

--- dist/js/script.js
+++ dist/js/script.js
@@ -36,5 +36,17 @@ const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
 
 darkToggle.addEventListener('click', function (){
-    darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'white';
+    }
 });
+
+if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)){
+    darkToggle.checked = true;
+}else{
+    darkToggle.checked = false;
+}
